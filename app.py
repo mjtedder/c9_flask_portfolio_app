ab18aceaa225c1bdf9de0e41b8e408d4d1aaea7f
mjtedder <36042608+mjtedder@users.noreply.github.com>
2018-02-03 12:41:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	new_name = str(name) + "likes to mix beats"
+	new_name = str(name) + " likes to mix beats"
 	return render_template('index.html', name=new_name)
 
 
